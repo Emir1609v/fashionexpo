f3b051b153484b76938207e9e27ac3bb514fd3da
Djasonvdh <524849@vistacollege.nl>
2025-06-10 11:49:13 +0200

    eerste stappen login form
    kaas is lekker
    gr. erik

---
+++ form.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+import dotenv
+import os
+
+# Load environment variables from .env file
+dotenv.load_dotenv()
+
+# Get the MongoDB connection string from environment variables
+MONGO_URI = os.getenv("D_MONGO_URI")
+if not MONGO_URI:
+    raise ValueError("MONGO_URI environment variable is not set.")
+
+# Connect to MongoDB
+client = MongoClient(MONGO_URI)
+
+# Check if the connection is successful
+try:
+    client.admin.command('ping')
+    print("Connected to MongoDB successfully.")
+except Exception as e:
+    print(f"Could not connect to MongoDB: {e}")
+    exit(1)  # Exit the program if the connection fails
+
+# Access the database
+db = client.get_database("fashionexpo")
+
+users_collection = db.get_collection("users")
+
+users_collection.create_index("email", unique=True)
+users_collection.create_index("username", unique=True)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("../templates/register.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
