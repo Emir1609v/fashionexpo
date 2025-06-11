ac653389c2f6bbfff452ff249bc2feda61fe0a5f
Djasonvdh <524849@vistacollege.nl>
2025-06-11 10:14:21 +0200

    venv gemaakt & eerste stappen mongo db

--- form.py
+++ form.py
@@ -35,7 +35,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("../templates/register.html")
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
